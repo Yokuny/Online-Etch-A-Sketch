6900e668642453fd6465195a4a27c7fff99246c1
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-14 15:32:04 -0300

    i made it happen
    before I create the element inside the canva I put there a variable Id. and a event listener to a hover. and whem i hover the element they send my there id and i get it and chance there background color

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvaElement = document.getElementById("canva");
 let squareSize = 24;
 let canvaSize = squareSize * squareSize;
+function colorChanger(elementId){
+    let theElementHovered = document.getElementById(elementId);
+    theElementHovered.style.backgroundColor = `rgb(190, 102, 129)`;
+
+}
 for (let index = 0; index < canvaSize; index++) {
     var newDivElement = document.createElement('div');
     newDivElement.className = 'squareProperty';
     newDivElement.style.cssText = `height: ${squareSize}px;`;
     newDivElement.style.cssText += `width: ${squareSize}px;`;
+    newDivElement.setAttribute('id', `squareNum${index}`);
+    newDivElement.addEventListener('mouseover', function(){
+        colorChanger(this.id);
+      });
+
     canvaElement.appendChild(newDivElement);
+    
+
+    
 }
