a8031fbf30b42395803a5ec63ca8b40c4c88898b
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-14 22:10:00 -0300

    square amount implemented
    now the user can choice the square size

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-let canvaElement = document.getElementById("canva");
-let squareSize = 24;
-let canvaSize = squareSize * squareSize;
+
 function colorChanger(elementId){
     let theElementHovered = document.getElementById(elementId);
     let stringBackgroundColor = theElementHovered.style.backgroundColor;
-    
-    console.log(stringBackgroundColor);
-
+    let newColorIntensity;
+    if((parseInt(stringBackgroundColor[16])) == 9){
+        newColorIntensity = 9;
+    }else{
+        newColorIntensity = parseInt(stringBackgroundColor[16]) +1;
+    }
+    let stringBackgroundModified = `rgba(0, 0, 0, 0.${newColorIntensity})`;
+    theElementHovered.style.backgroundColor = stringBackgroundModified;
 }
-for (let index = 0; index < canvaSize; index++) {
-    var newDivElement = document.createElement('div');
-    newDivElement.className = 'squareProperty';
-    newDivElement.style.cssText = `height: ${squareSize}px;`;
-    newDivElement.style.cssText += `width: ${squareSize}px;`;
-    newDivElement.style.cssText += `background-color: rgba(0, 0, 0, 0.0)`;
 
-    newDivElement.setAttribute('id', `squareNum${index}`);
-    newDivElement.addEventListener('mouseover', function(){
-        colorChanger(this.id);
-      });
-
-    canvaElement.appendChild(newDivElement);
+function squareGenerate(){
+    let canvaElement = document.getElementById("canva");
+    let squareAmount = document.getElementById("squareAmount").valueAsNumber;
+    canvaElement.innerHTML = '';
+    for (let index = 0; index < squareAmount*squareAmount; index++) {
+        var newDivElement = document.createElement('div');
+        newDivElement.className = 'squareProperty';
+        newDivElement.style.cssText = `height: 24px;`;
+        newDivElement.style.cssText += `width: 24px;`;
+        newDivElement.style.cssText += `background-color: rgba(0, 0, 0, 0.01)`;
     
-
-    
+        newDivElement.setAttribute('id', `squareNum${index}`);
+        newDivElement.addEventListener('mouseover', function(){
+            colorChanger(this.id);
+          });
+        canvaElement.appendChild(newDivElement);
+    }
 }
